e98050942fc6a4824ed4af4497ae8154e81914e6
Zuha Sharaz <zuhasharaz@Zuhas-MacBook-Pro.local>
2018-10-01 16:25:17 +0100

    working through symbols testing

--- the-language/spec/symbols_spec.rb
+++ the-language/spec/symbols_spec.rb
@@ -9,48 +9,48 @@ RSpec.describe "A symbol in Ruby" do
     the_same_symbol = :a_symbol
     another_symbol  = :another_symbol
 
-    expect( a_symbol == the_same_symbol ).to eq( __ )
-    expect( a_symbol == another_symbol ).to eq( __ )
+    expect( a_symbol == the_same_symbol ).to eq(true)
+    expect( a_symbol == another_symbol ).to eq( false)
   end
 
   it "is a single, internal object" do
     a_symbol        = :a_symbol
     the_same_symbol = :a_symbol
 
-    expect( a_symbol.object_id == the_same_symbol.object_id ).to eq( __ )
+    expect( a_symbol.object_id == the_same_symbol.object_id ).to eq( true )
   end
 
   it "is created for every method name" do
     symbols_as_strings = Symbol.all_symbols.map { |symbol| symbol.to_s }
 
     # split is a method on String that we have used previously
-    expect( symbols_as_strings.include?("split") ).to eq( __ )
+    expect( symbols_as_strings.include?("split") ).to eq( true)
   end
 
   MagicNumber = 3
   it "is created for every constant" do
     symbols_as_strings = Symbol.all_symbols.map { |symbol| symbol.to_s }
 
-    expect( symbols_as_strings.include?("MagicNumber") ).to eq( __ )
+    expect( symbols_as_strings.include?("MagicNumber") ).to eq( true )
   end
 
   it "can be created from a string" do
     string = "helloWorld"
 
-    expect( string.to_sym ).to eq( __ )
+    expect( string.to_sym ).to eq(:helloWorld )
   end
 
   it "can be created with spaces" do
     symbol = :"hello world"
 
-    expect( symbol ).to eq( __ )
+    expect( symbol ).to eq(:"hello world")
   end
 
   it "can be created with interpolated values" do
     value = "world"
     symbol = :"hello #{value}"
 
-    expect( symbol ).to eq( __ )
+    expect( symbol ).to eq(:"hello world")
   end
 
   it "can be interpolated into a string" do
