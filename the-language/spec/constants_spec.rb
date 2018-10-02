16cbfb7700e6ee71a3ba93d40cb7b67993bea66b
Simon Levy <simon.levy@onthebeach.co.uk>
2018-10-02 14:40:47 +0100

    truthyfalsey

--- the-language/spec/constants_spec.rb
+++ the-language/spec/constants_spec.rb
@@ -15,16 +15,16 @@ end
 describe "Ruby Constant" do
 
   it "references local constants" do
-    expect( More.local ).to eq( __ )
+    expect( More.local ).to eq( "nested" )
   end
 
   it "uses double colon to reference global constants" do
-    expect( More.double_colon ).to eq( __ )
+    expect( More.double_colon ).to eq( "top level" )
   end
 
   it "references nested constants using their complete path" do
-    expect( More::C ).to eq( __ )
-    expect( ::More::C ).to eq( __ )
+    expect( More::C ).to eq( "nested" )
+    expect( ::More::C ).to eq( "nested" )
   end
 
   class Animal
@@ -41,7 +41,7 @@ describe "Ruby Constant" do
   end
 
   it "inherits constants from enclosing classes" do
-    expect( Animal::NestedAnimal.new.legs_in_nested_animal ).to eq( __ )
+    expect( Animal::NestedAnimal.new.legs_in_nested_animal ).to eq( 4 )
   end
 
   class Reptile < Animal
@@ -51,13 +51,14 @@ describe "Ruby Constant" do
   end
 
   it "inherits constants in subclasses from the parent class" do
-    expect( Reptile.new.legs_in_reptile ).to eq( __ )
+    expect( Reptile.new.legs_in_reptile ).to eq( 4 )
   end
 
   class MyAnimals
-    LEGS = 2
-
+    LEGS = 4
     class Bird < Animal
+      LEGS = 2
+
       def legs_in_bird
         LEGS
       end
@@ -65,17 +66,17 @@ describe "Ruby Constant" do
   end
 
   it "can add a new constant in a subclass" do
-    expect( MyAnimals::Bird.new.legs_in_bird ).to eq( __ )
+    expect( MyAnimals::Bird.new.legs_in_bird ).to eq( 2 )
   end
 
   class MyAnimals::Oyster < Animal
     def legs_in_oyster
-      LEGS
+      MyAnimals::Oyster::LEGS
     end
   end
 
   it "is different when the lexical scope changes" do
-    expect( MyAnimals::Oyster.new.legs_in_oyster ).to eq( __ )
+    expect( MyAnimals::Oyster.new.legs_in_oyster ).to eq( 4 )
   end
 end
 
--- the-language/spec/control_statements_spec.rb
+++ the-language/spec/control_statements_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Control statements in ruby" do
     else
       result = :false_value
     end
-    expect( result ).to eq( __ )
+    expect( result ).to eq( :true_value )
   end
 
   it "has if then statements" do
@@ -14,7 +14,7 @@ RSpec.describe "Control statements in ruby" do
     if true
       result = :true_value
     end
-    expect( result ).to eq( __ )
+    expect( result ).to eq( :true_value )
   end
 
   it "returns a value from an if statement" do
@@ -23,14 +23,14 @@ RSpec.describe "Control statements in ruby" do
             else
               :false_value
             end
-    expect( value ).to eq( __ )
+    expect( value ).to eq( :true_value )
 
     value = if false
               :true_value
             else
               :false_value
             end
-    expect( value ).to eq( __ )
+    expect( value ).to eq( :false_value )
 
     # NOTE: Actually, EVERY statement in Ruby will return a value, not
     # just if statements.
@@ -40,24 +40,24 @@ RSpec.describe "Control statements in ruby" do
     value = if false
               :true_value
             end
-    expect( value ).to eq( __ )
+    expect( value ).to eq( nil )
   end
 
   it "has a condition (ternary) operator" do
     expect(
       true ? :true_value : :false_value
-    ).to eq( __ )
+    ).to eq( :true_value )
 
     expect(
       false ? :true_value : :false_value
-    ).to eq( __ )
+    ).to eq( :false_value )
   end
 
   it "can use an if as a statement modifier" do
     result = :default_value
     result = :true_value if true
 
-    expect( result ).to eq( __ )
+    expect( result ).to eq( :true_value )
   end
 
   it "has an unless statement" do
@@ -65,20 +65,20 @@ RSpec.describe "Control statements in ruby" do
     unless false    # same as saying 'if !false', which evaluates as 'if true'
       result = :false_value
     end
-    expect( result ).to eq( __ )
+    expect( result ).to eq( :false_value )
 
     result = :default_value
     unless true    # same as saying 'if !true', which evaluates as 'if false'
       result = :true_value
     end
-    expect( result ).to eq( __ )
+    expect( result ).to eq( :default_value )
   end
 
   it "can use unless as a statement modifier" do
     result = :default_value
     result = :false_value unless false
 
-    expect( result ).to eq( __ )
+    expect( result ).to eq( :false_value )
   end
 
   it "has a while statment" do
@@ -88,7 +88,7 @@ RSpec.describe "Control statements in ruby" do
       result = result * i
       i += 1
     end
-    expect( result ).to eq( __ )
+    expect( result ).to eq( 3628800 )
   end
 
   it "has a break statement" do
@@ -99,7 +99,7 @@ RSpec.describe "Control statements in ruby" do
       result = result * i
       i += 1
     end
-    expect( result ).to eq( __ )
+    expect( result ).to eq( 3628800 )
   end
 
   it "can return a value with break" do
@@ -109,7 +109,7 @@ RSpec.describe "Control statements in ruby" do
       i += 1
     end
 
-    expect( result ).to eq( __ )
+    expect( result ).to eq( 2 )
   end
 
   it "can skip on using next" do
@@ -120,7 +120,7 @@ RSpec.describe "Control statements in ruby" do
       next if (i % 2) == 0
       result << i
     end
-    expect( result ).to eq( __ )
+    expect( result ).to eq( [1, 3, 5, 7, 9] )
   end
 
   it "has a for statement" do
@@ -129,7 +129,7 @@ RSpec.describe "Control statements in ruby" do
     for item in array
       result << item.upcase
     end
-    expect( result ).to eq( [__, __, __] )
+    expect( result ).to eq( ["FISH","AND", "CHIPS"] )
   end
 
   it "has a times statement" do
@@ -137,7 +137,7 @@ RSpec.describe "Control statements in ruby" do
     10.times do
       sum += 1
     end
-    expect( sum ).to eq( __ )
+    expect( sum ).to eq( 10 )
   end
 
 end
