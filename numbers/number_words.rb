58a7643f1a02982e8fab2c0dde7106a2f44c0090
Zuha Sharaz <zuhasharaz@Zuhas-MacBook-Pro.local>
2018-10-05 13:26:05 +0100

    tests passing for teens

--- numbers/number_words.rb
+++ numbers/number_words.rb
@@ -1,9 +1,12 @@
 def numbers_to_words(num)
 
 singlenums = %w[zero one two three four five six seven eight nine]
+teens = %w[ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen]
+
 case num
 
 when 0..9 then singlenums[num %10]
+when 10..19 then teens[num%10]
 
 end
 
--- numbers/spec/number_words_spec.rb
+++ numbers/spec/number_words_spec.rb
@@ -2,10 +2,15 @@ require_relative '../number_words'
 
 RSpec.describe "" do
 	context "Single number to string" do
-        it "should return the string for 0" do
+        it "should return the string for single numbers" do
             expect(numbers_to_words(0)).to eq('zero')
             expect(numbers_to_words(5)).to eq('five')  
             expect(numbers_to_words(9)).to eq('nine') 
+        end
+        it "should return the string for teens" do
+            expect(numbers_to_words(10)).to eq('ten')
+            expect(numbers_to_words(15)).to eq('fifteen')
+            expect(numbers_to_words(19)).to eq('nineteen')
         end
 	end
 end
